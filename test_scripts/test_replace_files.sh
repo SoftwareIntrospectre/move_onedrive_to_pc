e1e6e77baa0b519682e0037abb152a80d88cac18
SoftwareIntrospectre <softwareintrospectre@gmail.com>
2022-12-28 08:26:35 -0500

    Experimenting with additional functionality. Not ready for Master.

--- test_scripts/test_replace_files.sh
+++ test_scripts/test_replace_files.sh
@@ -5,17 +5,36 @@ local_file=$(find ~/Documents -type f -name 'test_File_For_BASH.txt')
 oneDrive_file=$(find ~/OneDrive/Documents -type f -name 'test_File_For_BASH.txt')
 
 
-overwriteLocal(){
-	echo "They differ"
-	cat "$oneDrive_file"  > "$local_file"
-	echo "OneDrive file overwriting Local File"
+updateLocalFileToCurrentVersion(){
+	#check to see which is newer. Use that one to update
+
+	echo "They differ. Checking to see which is newer."
+	
+	if "$oneDrive_file" -nt "$local_file"
+	then 
+		echo "OneDrive file is newer. Overwriting local."
+		cat "$oneDrive_file"  > "$local_file"
+		deleteFile "$oneDrive_file"
+	
+	elif $local_file -nt $oneDrive_File
+	then
+		echo "Local is newer. Deleting OneDrive file."
+		deleteFile "$oneDrive_file"
+		
+	else
+		echo "Exception case inner"
+	fi
 }
 
 
+deleteFile(){
+	echo "deleting $($1)"
+	rm $1
+}
 
-if !  diff -rq "$local_file" "$oneDrive_file";
+if ! diff -rq "$local_file" "$oneDrive_file" 
 then
-	overwriteLocal "$local_File" "$oneDrive_file"
+	updateLocalFileToCurrentVersion "$local_File" "$oneDrive_file"
 else
-	echo "Local File not overwritten. Newer."
+	echo "No differences."
 fi
